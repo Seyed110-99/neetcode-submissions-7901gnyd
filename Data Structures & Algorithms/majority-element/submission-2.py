b17deb36aefb7395757fb92c189cca39a143fad3
Khash <186739011+Seyed110-99@users.noreply.github.com>
2026-04-07 11:26:44 +0100

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        items = {}
+        n = len(nums)
+        majority = None
+        for x in nums:
+
+            items[x] = items.get(x, 0) + 1
+
+        for key, value in items.items():
+
+            if items[key] >= n/2:
+                majority = key
+        
+        return majority
+        
