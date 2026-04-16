b8a7e836568010b029d97e576d294851dfae7375
Khash <186739011+Seyed110-99@users.noreply.github.com>
2026-04-16 16:08:29 +0100

    Add: valid-palindrome-ii - submission-9

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-9.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+
+        def check(arr, l, r):
+
+            while l < r:
+                if arr[l] != arr[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True 
+
+        l = 0
+        r = len(s)-1
+
+        while l < r:
+
+            if s[l] != s[r]:
+                return check(s, l+1, r) or check(s, l, r-1)
+            l += 1
+            r -= 1
+        return True
+
+            
+
+                
