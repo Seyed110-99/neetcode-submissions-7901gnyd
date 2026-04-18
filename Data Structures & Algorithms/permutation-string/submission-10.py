39eb8a2a5e541778d0429980f70ba197d6f85629
Khash <186739011+Seyed110-99@users.noreply.github.com>
2026-04-18 13:15:39 +0100

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,33 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        if len(s1) > len(s2):
+            return False 
+        
+        
+        perm = [0 for _ in range(26)]
+
+        for char in s1:
+            x = ord(char) - ord("a")
+            perm[x] += 1
+
+        l = 0
+        window = [0 for _ in range(26)]
+
+        for r in range(len(s2)):
+                
+            x = ord(s2[r]) - ord("a")
+            window[x] += 1
+                
+            if r - l + 1 == len(s1):
+                if window == perm:
+                    return True
+                else:
+                    p = ord(s2[l]) - ord("a")
+                    window[p] -= 1
+                    l += 1
+
+        return False
+            
+            
+
